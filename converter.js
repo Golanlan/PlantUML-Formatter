9f13ff42e8a487843a5ac7204c51eee3ed93a02d
Golanlan <30702459+Golanlan@users.noreply.github.com>
2023-05-25 08:05:45 +0300

    Update converter.js

--- converter.js
+++ converter.js
@@ -1,45 +1,47 @@
 function formatContent(content) {
-  let indentLevel = 0;
-  let isNoteBlock = false;
-  let result = '';
+    let indentLevel = 0;
+    let isNoteBlock = false;
+    let result = '';
 
-  for (let line of content.split('\n')) {
-    const trimmedLine = line.trim();
+    for (let line of content.split('\n')) {
+        const trimmedLine = line.trim();
 
-    if (trimmedLine === '') continue;
+        if (trimmedLine === '') continue;
 
-    // Handle end of blocks
-    if (trimmedLine.startsWith('end') || trimmedLine === '}') {
-      if (isNoteBlock && trimmedLine === 'end note') isNoteBlock = false;
-      indentLevel = Math.max(indentLevel - 1, 0);
+        // Handle end of blocks
+        if (trimmedLine.startsWith('end') || trimmedLine === '}') {
+            if (isNoteBlock && trimmedLine === 'end note') isNoteBlock = false;
+            indentLevel = Math.max(indentLevel - 1, 0);
+        }
+
+        // Add indentation to the current line
+        result += '    '.repeat(indentLevel) + trimmedLine + '\n';
+
+        // Handle start of blocks
+        if (
+            trimmedLine.startsWith('group') ||
+            trimmedLine.startsWith('alt') ||
+            trimmedLine.startsWith('opt') ||
+            trimmedLine.startsWith('skinparam') ||
+            trimmedLine === '{' ||
+            (trimmedLine.startsWith('note') && !trimmedLine.endsWith('note') && !trimmedLine.includes(':'))
+        ) {
+            indentLevel++;
+            if (trimmedLine.startsWith('note')) isNoteBlock = true;
+        }
     }
 
-    // Add indentation to the current line
-    result += '    '.repeat(indentLevel) + trimmedLine + '\n';
-
-    // Handle start of blocks
-    if (
-      trimmedLine.startsWith('group') ||
-      trimmedLine.startsWith('alt') ||
-      trimmedLine.startsWith('opt') ||
-      trimmedLine.startsWith('skinparam') ||
-      trimmedLine === '{' ||
-      (trimmedLine.startsWith('note') && !trimmedLine.endsWith('note') && !trimmedLine.includes(':'))
-    ) {
-      indentLevel++;
-      if (trimmedLine.startsWith('note')) isNoteBlock = true;
-    }
-  }
-
-  return result;
+    return result;
 }
 
-const convertBtn = document.getElementById("convertBtn");
-const codeInput = document.getElementById("codeInput");
-const convertedCode = document.getElementById("convertedCode");
+window.onload = function() {
+    const convertBtn = document.getElementById("convertBtn");
+    const codeInput = document.getElementById("codeInput");
+    const convertedCode = document.getElementById("convertedCode");
 
-convertBtn.addEventListener("click", () => {
-  const inputCode = codeInput.value;
-  const converted = formatContent(inputCode);
-  convertedCode.value = converted;
-});
+    convertBtn.addEventListener("click", function() {
+        const inputCode = codeInput.value;
+        const converted = formatContent(inputCode);
+        convertedCode.value = converted;
+    });
+}
